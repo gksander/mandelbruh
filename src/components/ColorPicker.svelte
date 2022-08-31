bf168c4b7729d3bdcf6a3d01bc1b91994b76ea5b
Grant Sander <gksander93@gmail.com>
2022-08-31 07:34:01 -0500

    Dark mode styles

--- src/components/ColorPicker.svelte
+++ src/components/ColorPicker.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <label
-	class="flex gap-2 items-center cursor-pointer px-1 py-1.5 hover:bg-gray-100 rounded transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+	class="flex gap-2 items-center cursor-pointer px-1 py-1.5 hover:bg-gray-100 dark:hover:bg-gray-600 rounded transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
 	tabindex="0"
 >
 	<input
@@ -19,5 +19,5 @@
 		}}
 		class="w-8 h-6 rounded bg-gray-100"
 	/>
-	<div class="flex-grow text-sm font-medium text-gray-900">{description}</div>
+	<div class="flex-grow text-sm font-medium text-gray-900 dark:text-gray-300">{description}</div>
 </label>
--- src/components/Controls.svelte
+++ src/components/Controls.svelte
@@ -7,55 +7,57 @@
 	};
 </script>
 
-<div class="bg-white rounded-md shadow-lg w-64 overflow-hidden">
-	<h1 class="text-3xl text-gray-700 p-3">Mandelbruh</h1>
+<div class="bg-white dark:bg-gray-700 rounded-md shadow-lg w-64 overflow-hidden">
+	<div class="grid gap-3 p-3">
+		<h1 class="text-3xl text-gray-700 dark:text-gray-300">Mandelbruh</h1>
 
-	<div class="p-3">
-		<div class="grid gap-3">
-			<div>
-				<label
-					for="iterations"
-					class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
-					>Iterations ({Math.round($iterations)})</label
-				>
-				<input
-					id="iterations"
-					type="range"
-					min="10"
-					max="600"
-					step="2"
-					bind:value={$iterations}
-					class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-				/>
-			</div>
-
-			<div>
-				<ColorPicker color={boundColor} description="Bound color" />
-				<ColorPicker color={transitionColor} description="Transition color" />
-				<ColorPicker color={escapeColor} description="Escape color" />
-			</div>
-
-			<button
-				on:click={onReset}
-				class="text-sm rounded border px-3 py-1.5 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 transition-colors duration-200"
-				>Reset</button
-			>
+		<div class="prose text-sm text-gray-700 leading-snug dark:text-gray-100">
+			Click and drag to pan. Scroll to zoom. Use controls below to adjust iterations/colors.
 		</div>
+
+		<div>
+			<label
+				for="iterations"
+				class="block mb-1 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>Iterations ({Math.round($iterations)})</label
+			>
+			<input
+				id="iterations"
+				type="range"
+				min="10"
+				max="600"
+				step="2"
+				bind:value={$iterations}
+				class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-600"
+			/>
+		</div>
+
+		<div>
+			<ColorPicker color={boundColor} description="Bound color" />
+			<ColorPicker color={transitionColor} description="Transition color" />
+			<ColorPicker color={escapeColor} description="Escape color" />
+		</div>
+
+		<button
+			on:click={onReset}
+			class="text-sm rounded border dark:border-gray-500 px-3 py-1.5 bg-white dark:bg-gray-600 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 transition-colors duration-200"
+			>Reset</button
+		>
 	</div>
 
-	<div class="border-t flex text-xs font-medium">
+	<div class="border-t flex text-xs font-medium dark:text-gray-200">
 		<a
 			href="https://github.com/gksander"
 			target="_blank"
 			rel="noreferrer"
-			class="flex-1 text-center border-r p-3 cursor-pointer bg:white hover:bg-gray-100 transition-colors duration-150"
+			class="flex-1 text-center border-r p-3 cursor-pointer bg:white hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-150"
 			>@gksander</a
 		>
 		<a
 			href="https://github.com/gksander/mandelbruh"
 			target="_blank"
 			rel="noreferrer"
-			class="flex-1 text-center p-3 cursor-pointer bg:white hover:bg-gray-100 transition-colors duration-150"
+			class="flex-1 text-center p-3 cursor-pointer bg:white hover:bg-gray-100 dark:hover:bg-gray-600 transition-colors duration-150"
 			>GitHub</a
 		>
 	</div>
